d2758751c6f41e666e60077d65abdc181b6f8f4c
Yash Vardhan Choudhary <90108719+Y45H-GitHub@users.noreply.github.com>
2024-11-11 17:15:04 +0530

    Quiz App Backend

---
+++ QuizAppBackend/QuizApp/src/main/java/com/example/QuizApp/service/QuestionService.java
@@ -0,0 +1,26 @@
+package com.example.QuizApp.service;
+
+import com.example.QuizApp.entity.QuizQuestion;
+import com.example.QuizApp.repo.QuestionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionRepo questionRepo;
+
+    public List<QuizQuestion> getAllQuestions()
+    {
+        List<QuizQuestion> questionRepoAll = questionRepo.findAll();
+        return questionRepoAll;
+    }
+
+    public QuizQuestion saveQuestion(QuizQuestion question){
+        QuizQuestion save = questionRepo.save(question);
+        return save;
+    }
+}
